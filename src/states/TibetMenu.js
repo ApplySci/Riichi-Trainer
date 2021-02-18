4eee35152181ab925ead544ab19ea06082539ada
Apply_Sci <mj.apply.sci@gmail.com>
2021-02-18 07:13:07 +0000

    remove popup

--- src/states/TibetMenu.js
+++ src/states/TibetMenu.js
@@ -3,27 +3,16 @@ import { Container } from 'reactstrap';
 import { withTranslation } from "react-i18next";
 import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
 import Tibet from "./Tibet";
-import PopUp from "../components/PopUp";
 import Intro from "../components/Intro";
 import SichuanClient from "./SichuanClient";
 
 class TibetMenu extends React.Component {
-    state = {
-        showPopup: false
-        };
-
-    togglePop = () => {
-        this.setState({
-            showPopup: !this.state.showPopup
-        });
-    };
 
     render() {
         return (
             <BrowserRouter>
-                {this.state.showPopup ? <PopUp toggle={this.togglePop} /> : null}
                 <nav>
-                    <Link to=''><button className="metal radial" onClick={this.togglePop}>牛</button></Link>
+                    <Link to='/'><button className="metal radial">牛</button></Link>
                     <ul>
                         <li><NavLink to='/tibet/5/1/0/0/0'>5 tiles, 1 suit</NavLink></li>
                         <li><NavLink to='/tibet/8/1/0/0/0'>8 tiles, 1 suit</NavLink></li>
